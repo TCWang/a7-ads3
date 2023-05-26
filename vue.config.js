d51be98574b7c16e34b08b26dac1adb2aea0b201
TC Wang <wangtct@hotmail.com>
2023-05-26 10:55:39 -0700

    add publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: "a7-ads3",
+  transpileDependencies: true,
+});
