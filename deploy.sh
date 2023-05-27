fe772d1277d24acc02c6a8e1a572fdad0dfa3425
TC Wang <wangtct@hotmail.com>
2023-05-27 09:46:30 -0700

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:TCWang/a7-ads3.git main:gh-pages
 
 cd -
